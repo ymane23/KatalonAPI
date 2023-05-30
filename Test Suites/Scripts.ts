774b1bc590f46e6662e023d24faeda7ea1509b97
Yogesh Mane <ymane@dc2.propsol.org>
2023-05-30 16:19:13 +0530

    4th commit

--- Test Suites/Scripts.ts
+++ Test Suites/Scripts.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>cd4ba37c-4f98-43fa-aa13-36872e1b03c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b05403ac-6658-494c-bfe3-a54f279c407d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/script</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
