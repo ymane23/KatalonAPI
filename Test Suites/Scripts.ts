75b86ee3e2035d664445224032f91973fe3321a1
Yogesh Mane <ymane@dc2.propsol.org>
2023-05-30 16:08:25 +0530

    third commit

---
+++ Test Suites/Scripts.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Scripts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cd4ba37c-4f98-43fa-aa13-36872e1b03c4</testSuiteGuid>
+</TestSuiteEntity>
