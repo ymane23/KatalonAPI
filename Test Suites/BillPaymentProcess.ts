1e1c8d46657744a2858dda93c5f7a07706d22bb1
Yogesh Mane <ymane@dc2.propsol.org>
2023-05-31 12:21:27 +0530

    commit

---
+++ Test Suites/BillPaymentProcess.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BillPaymentProcess</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e0d61d80-59ea-458c-a5d4-6358d38fd82c</testSuiteGuid>
+   <testCaseLink>
+      <guid>3304e13f-fe85-455d-ad55-0925abb88905</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BillPaymentProcess</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
